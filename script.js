7fe0fe3d8814fddc5f3b04993210fe2a95a8be7c
anthony-winchell <123203535+anthony-winchell@users.noreply.github.com>
2025-07-24 00:14:27 -0400

    form fade

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const supabaseUrl = 'https://asyqeocahphppyxnawkl.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzeXFlb2NhaHBocHB5eG5hd2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxNjEwNzIsImV4cCI6MjA2ODczNzA3Mn0.DxDjjxeN8029aXy-mAtfVegnDz2ln4ZkovHDyTtSFjM'; // replace with your actual anon key
 const supabaseClient = createClient(supabaseUrl, supabaseKey);
 
-//form submission handler
+
+document.addEventListener('DOMContentLoaded', () => {
+  const today = new Date().toISOString().split('T')[0];
+  document.getElementById('startDate').value = today;
+});
 
 const form = document.getElementById('serviceRequestForm');
 const formSection = document.getElementById('formSection');
+
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
 
@@ -39,13 +44,21 @@ form.addEventListener('submit', async (event) => {
     console.error('Error inserting data:', error);
     alert('Failed to submit your request. Please try again.');
   } else {
-    alert('Request submitted successfully!');
-    form.reset();
+    // Smoothly fade out form and replace with confirmation
+    form.style.transition = 'opacity 0.5s ease';
+    form.style.opacity = '0';
+
+    setTimeout(() => {
+      form.innerHTML = form.innerHTML = `
+      <div style="display: flex; justify-content: center; align-items: center; gap: 0.5rem; flex-direction: column">
+      <img src="./assets/check.png" height="100" width="100" alt="Checked Icon" style="margin: 20px 0 10px 0;/>
+        <p style="font-size: 3rem;">
+          You're all set! Someone will be in touch soon.
+        </p>
+      </div>
+    `;
+    
+      form.style.opacity = '1';
+    }, 500);
   }
 });
-
-//set the default value for the start date to today
- document.addEventListener('DOMContentLoaded', () => {
-    const today = new Date().toISOString().split('T')[0];
-    document.getElementById('startDate').value = today;
-   });
